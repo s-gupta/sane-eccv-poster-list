506997143be1f1c7d27c714c6c5d899181618b81
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2020-08-26 12:10:57 -0500

    paper data

---
+++ script.py
@@ -0,0 +1,48 @@
+from html4vision import Col, imagetable
+
+def load_file(file_name):
+    ls = []
+    with open(file_name) as f:
+        for l in f:
+            ls.append(l.rstrip())
+    return ls
+
+def load_poster_data():
+    authors = load_file('authors.txt') 
+    links = load_file('links2.txt') 
+    titles = load_file('title.txt') 
+    poster_id = load_file('poster-id.txt')
+    links = [f'https://papers.eccv2020.eu/paper/{x}/' for x in links]
+    return authors, links, titles, poster_id
+
+def load_session_posters(session):
+    ls = []
+    with open(session + '.pretty') as f:
+        for l in f:
+            ls.append(int(l))
+    return ls
+
+def generate_file(out_file_name, authors, links, titles, poster_id, select_ids):
+    ls = []
+    # import pdb; pdb.set_trace()
+    for i in range(len(authors)):
+        if int(poster_id[i]) in select_ids:
+            ls.append(i)
+    print(ls)
+    authors = [authors[x] for x in ls]
+    titles = [titles[x] for x in ls]
+    poster_id = [poster_id[x] for x in ls]
+    links = [links[x] for x in ls]
+    cols = [Col('text', 'ID', poster_id), 
+            Col('text', 'Title', titles, href=links), 
+            Col('text', 'Authors', authors)]
+    imagetable(cols, out_file_name, sortable=True, style="td {text-align: left} table, th, td { border: 1px solid black;  border-collapse: collapse;   padding: 5px;}")
+
+def process():
+    authors, links, titles, poster_id = load_poster_data()
+    select_ids = load_session_posters('09')
+    out_file_name = '09-good.html'
+    generate_file(out_file_name, authors, links, titles, poster_id, select_ids)
+
+if __name__ == '__main__':
+    process()
